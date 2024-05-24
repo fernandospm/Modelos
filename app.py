17588cc6f36a9b55b21984c29287853a1dc3dac2
Fernando Sampayo <fersampperez@gmail.com>
2024-05-24 20:50:37 +0000

    actualizacion

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import requests
 while True:
     pregunta = input("PROMPt:")
     data = {
-        "model": "llama2",
+        "model": "llama3",
         "prompt": pregunta,
         "system":"Responde como Goku ssj2",
         "stream":False 
     }
 
-requests=requests.post('http://localhost:11434/api/generate',json=data)
+requests=requests.post('http://localhost:11434/api/generate', json=data)
 
 response= json.loads(response.text)
 print(response['response'])
