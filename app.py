3b8abd8a7f5c82ca1a9e077a6eba5556a41e8fe5
Fernando Sampayo <fersampperez@gmail.com>
2024-05-24 20:49:52 +0000

    actualizacion

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+while True:
+    pregunta = input("PROMPt:")
+    data = {
+        "model": "llama2",
+        "prompt": pregunta,
+        "system":"Responde como Goku ssj2",
+        "stream":False 
+    }
+
+requests=requests.post('http://localhost:11434/api/generate',json=data)
+
+response= json.loads(response.text)
+print(response['response'])
